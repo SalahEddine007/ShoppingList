12da7f6662e6360c5c6148dcf2797ebec1f4540c
Salah Eddine <salahdin.boukerkour@gmail.com>
2019-02-26 23:37:03 +0100

    make it better

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+   mongoURI: 'mongodb+srv://salah_list:ismail_2014@listitems-zotn2.mongodb.net/test?retryWrites=true',
+ 
+   
+
+    // mongoURI: 'mongodb+srv://salaheddine:ismail_2014@cluster0-ryphu.mongodb.net/admin'
+    // 196.77.39.75/32 
+    
+}
